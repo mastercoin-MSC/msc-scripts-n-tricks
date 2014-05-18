8e38bbcfeac6146e1f059b48c264721090fb5f74
Faiz K <faizkhan00@gmail.com>
2014-05-18 17:30:27 -0500

    update script to output JSON, test condition

--- sx-roundrobin-keepalive.sh
+++ sx-roundrobin-keepalive.sh
@@ -1,5 +1,6 @@
 #Declare valid servers
 SX_CORE=/usr/local/lib/sx-core
+WWW_DIR=/tmp
 valid[0]='"tcp://obelisk.bysh.me:9091"'
 valid[1]='"tcp://obelisk.unsystem.net:9091"'
 valid[2]='"tcp://obelisk.unsystem.net:8081"'
@@ -8,10 +9,21 @@ valid[4]='"tcp://54.187.205.158:9091"'
 ACTIVEINDEX=4
 while true
 do
-  echo "Attempting connection with $(cat ~/.sx.cfg)..."
-  eval "$SX_CORE/sx-fetch-last-height > /dev/null 2>&1 &" 
+  echo "$(date) Connecting to: $(cat ~/.sx.cfg)"
+  eval "$SX_CORE/sx-fetch-last-height 2> /dev/null &" > /tmp/last_height.tmp
   SXPID=$!
-  sleep 5
+  sleep 8
+
+  #Create test condition if return is success 
+  SX_TEST=$(cat /tmp/last_height.tmp)
+  if [[ -n $SX_TEST ]]
+  then
+    #Update block height
+    echo $SX_TEST > /tmp/block_height.tmp
+  fi
+  #Get last height
+  SX_BLOCK=$(cat /tmp/block_height.tmp)
+  #Check if sx is still running
   SUCCESS=$($(kill -0 $SXPID > /dev/null 2>&1); echo $?)
   #echo "PID IS $SXPID SUCCESS IS $SUCCESS"
   if [[ $SUCCESS -eq 0 ]]; then
@@ -19,20 +31,19 @@ do
     disown $SXPID
     kill -9 $SXPID > /dev/null 2>&1 #kill sx fetch-last-height
 
-    #echo "ACTIVEINDEX IS $ACTIVEINDEX" 
-    if [[ $ACTIVEINDEX -lt ${#valid[*]} ]]
+    if [[ $ACTIVEINDEX -lt ${#valid[*]} ]] #this is the index of the array above
     then
     #Kill SX proc for taking too long
       echo "service=${valid[$ACTIVEINDEX]}" > ~/.sx.cfg
-      echo "Taking too long, switching server to $(cat ~/.sx.cfg)...\n"
+      echo "$(date) No response, seeking..."
+
+      echo "{ \"status\": \"DOWN\", \"last_known_height\": \""$SX_BLOCK"\" , \"heartbeat_timestamp\": \""$(date)"\" }" > $WWW_DIR/sx_conn.json
       ACTIVEINDEX=$(($ACTIVEINDEX+1))
     else 
     ACTIVEINDEX=0
     fi
   else
     echo "SX connection established."
-    #ACTIVE=$i
+    echo "{ \"status\": \"UP\", \"last_known_height\": \""$SX_BLOCK"\" , \"heartbeat_timestamp\": \""$(date)"\" }" > $WWW_DIR/sx_conn.json
   fi
 done
-
-New Relic
